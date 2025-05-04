a247f66a441dd59c911c42153979dae8edca2aa3
a25109te <a25109te@aiit.ac.jp>
2025-05-04 05:22:41 +0000

    Finish ch03

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # GEt / => static_page#home
+  root 'static_pages#home'
+
+  # GEt /static_pages/home => static_pages#home
+  # GET /about => static_pages#aboutの方が見やすい
   get 'static_pages/home'
   get 'static_pages/help'
-  root "hello#index"
+  get 'static_pages/about'
 end
