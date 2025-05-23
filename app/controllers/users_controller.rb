842b5d60f38ed20e06773e0f3f862c32e0786781
a25109te <a25109te@aiit.ac.jp>
2025-05-23 11:42:24 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+
+    # => app/views/users/show.html.erb
+  end
+
+  # GET /users/new or /signup
   def new
-    # app/views/users/new.html.erb
+    @user = User.new
+    # => app/views/users/new.html.erb
+  end
+
+  # POST /users
+  def create
+    # params => user => user.save => if ... else ...end
+    # params[user[:name]] => 'foobar'
+    # params[user[:name]] => 'foobar@example.com'
+    # ...
+
+    @user = User.new(user_params)
+    if @user.save
+      # => Success / alert-success
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user # => GET /users/:id
+      # redirect_to_user_path(@user)
+    else
+      # => Failure
+      render 'new', status: :unprocessable_entity
+    end
+
+    # x render 'create'
+  end
+
+  private
+
+  # Strong Parameters
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
